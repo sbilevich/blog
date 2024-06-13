6ace00d934b1d02d9a448dec1a7a37e39a292e08
Svetlana Bilevich <svetlana_bilevich@epam.com>
2024-06-13 12:24:53 +0100

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => (
   <AppBar position="static" className="z-20">
     <Toolbar>
-      <Typography variant="h6">My Blog</Typography>
+      <Link to="/">
+        <Typography variant="h6" component="span">
+          My Blog
+        </Typography>
+      </Link>
     </Toolbar>
   </AppBar>
 );
